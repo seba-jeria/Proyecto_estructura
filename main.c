5aa77f43fb882d0c9a4ba7f6fb10fbebc23529e9
LoneMRP <sr.fenixd@gmail.com>
2022-11-16 21:51:18 -0300

    Menu y Estructuras

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include <math.h>
 #include <windows.h>
-
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hashmap.h"
 
 //Declaracion Struct
 
@@ -22,16 +24,94 @@ typedef struct{
 
     char Nombre_Item[30];
     char Tipo[30];
-    int Daño_Cantidad_Armadura;
+    int Dano_Cantidad_Armadura;
 
 }Objeto;
 
 typedef struct{
 
+    char Nombre_Enemigo[30];
     int Vida;
-    int Daño;
+    int Dano;
 
 }Enemigo;
 
+//Funcion para leer el k-esimo elemento de un string (separado por comas)
+char *get_csv_field(char *linea, int indice){
+    char *campo = (char *) malloc(100 * sizeof(char *)); // Guarda el string a retornar
+    int i = 0; // Recorre la linea
+    int k = 0; // Cuenta las comas
+    int n = 0; // Recorre el campo
+    bool coma = false;
+    while(linea[i] != '\0'){
+        if(linea[i] == '\"'){
+            coma = !coma;
+        }
+        if(k == indice){
+            if(linea[i] != '\"'){
+                campo[n] = linea[i];
+                n++;
+            }
+        }
+        i++;
+        if(linea[i] == ',' && !coma){
+            k++;
+            i++;
+        }
+        if(k > indice || linea[i] == '\0' || linea[i] == '\n'){
+            campo[n] = '\0';
+            return campo;
+        }
+    }
+    return NULL;
+}
+void menu();
+
+int main(){
+    int num;
+    bool flag = true;
+    menu();
+    scanf("%d",&num);
+    while(num<1||num>3){
+        printf("Introdusca un numero valido: ");
+        scanf("%d", &num);
+    }
+    while (flag){
+        switch (num){
+            case 0:{
+                printf("Programa finalizado");
+                flag=false;
+                break;
+            }
+	        case 1:{
+
+            }
+            case 2:{
+
+            }
+            case 3:{
+
+            }
+        }
+        if (num==3){
+            break;
+        }
+        printf("\n");
+        printf("Ingrese otro valor: ");
+        scanf("%d", &num);
+        while(num<1||num>3){
+            printf("Introdusca un numero valido: ");
+            scanf("%d", &num);
+        }
+    }
+return 0;
+}
+
+void menu(){
+    printf("1. Iniciar Partida\n");
+    printf("2. Ver Macadores\n");
+    printf("3. Salir\n");
+    printf("Introdusca un numero: ");
+}
 
 
