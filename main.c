6631fa07a54811777e9c3cce34db2fe4892f0f4d
seba.jeria <sebastian.jeria.l@mail.pucv.cl>
2022-11-23 19:07:18 -0300

    Estructura del grafo y funciones de cargar

--- main.c
+++ main.c
@@ -7,26 +7,17 @@
 #include "hashmap.h"
 #include "list.h"
 #include "treemap.h"
+//#include "combate.h"
+//#include "grafo.h"
 
 //Declaracion Struct
-
-/*struct NodeGrafo{
-    Nivel* Nivel;
-    List* NivelaDesbloquear;
-    bool Completado;
-};
-
-typedef struct{
-    Enemigo* e;
-    Personaje* p;
-    Objeto* o;
-}Nivel;*/
 
 typedef struct{
 
     char Nombre_Item[30];
     char Tipo[30];
     int Dano_Cantidad_Armadura;
+    int Nivel_o;
 
 }Objeto;
 
@@ -45,15 +36,39 @@ typedef struct{
     char Nombre_Enemigo[30];
     int Vida;
     int Dano;
+    int nivel_e;
 
 }Enemigo;
+
+typedef struct{
+
+    Enemigo* enemigo;
+    Objeto* objeto;
+    NodoGrafo* nodoizquierdo;
+    NodoGrafo* nododerecha;
+
+}NodoGrafo;
+
+typedef struct{
+    
+    HashMap* Niveles;
+    NodoGrafo* nodoinicial;
+    NodoGrafo* nodoactual;
+
+}Grafo;
+
+typedef struct{
+
+    char nombreNivel;
+    int camino_der;
+    int camino_izq;
+
+}Nivel;
 
 typedef struct {
     char nombre[30];
     int puntaje;
 }Usuario;
-
-
 
 //Funcion para leer el k-esimo elemento de un string (separado por comas)
 char *get_csv_field(char *linea, int indice){
@@ -84,11 +99,28 @@ char *get_csv_field(char *linea, int indice){
     }
     return NULL;
 }
-void menu();
 
+void menu();
+void submenu();
 int lower_than_int(void* , void* );
 void agregarVideojuego(TreeMap *, void* , Usuario*);
 void mostrar(TreeMap *, Pair *, Pair* (avanzar)(TreeMap *mapa));
+
+void avanzar();
+void Mostrarinventario();
+void Mostrarestado();
+bool esnemigo(NodoGrafo*);
+void combatir();
+void tomarobjeto();
+void mostrarinfoenemigo();
+void motrarinfoobjeto();
+
+//cargar objetos y enemigos, despues cargar los niveles
+void Cargarenemigos(HashMap*);
+void Cargarobjetos(HashMap*);
+void CargarNiveles(Grafo*);
+
+Grafo* grafo;
 
 
 int main(){
@@ -96,6 +128,12 @@ int main(){
     bool flag = true;
 
     TreeMap* Marcadores = createTreeMap(lower_than_int);
+    HashMap* Mapaenemigo = (Enemigo*)malloc(sizeof(Enemigo));
+    Cargarenemigos(Mapaenemigo);
+    HashMap* Mapaobjeto = (Objeto*)malloc(sizeof(Objeto));
+    Cargarobjetos(Mapaobjeto);
+    grafo = (Grafo*)malloc(sizeof(Grafo));
+    CargarNiveles(grafo);
 
     Usuario* u = (Usuario*)malloc(sizeof(Usuario));
     Personaje* p = (Personaje*)malloc(sizeof(Personaje));
@@ -109,45 +147,50 @@ int main(){
 
     while (flag){
         switch (num){
-            case 0:{
-                printf("Programa finalizado");
-                flag=false;
-                break;
-            }
 	        case 1:{
 
                 char ing_nombre;
+                int opcion, aux;
 
                 printf("Ingrese nombre personaje: ");
                 fflush(stdin);
                 scanf("%[^\n]", ing_nombre);
 
-                
+                submenu();
+                fflush(stdin);
+                scanf("%d", &opcion);
 
-                break;
+                while(1){
+                    switch (opcion){
+                        case 1:{
+                            avanzar();
+                            break;
+                        }
+                        case 2:{
+                            Mostrarinventario();
+                            break;
+                        }
+                        case 3:{
+                            Mostrarestado();
+                            break;
+                        }
+                        if (num==4){
+                            printf("Juego finalizado");
+                            break;
+                        }
+                        printf("\n");
+                        printf("Ingrese otro valor: ");
+                        scanf("%d", &aux);
+                        while(aux<1||aux>4){
+                            printf("Introdusca un numero valido: ");
+                            scanf("%d", &aux);
+                        }
+                    }
+                }
             }
             case 2:{
 
-                int k=0;
                 int opcion;
-
-                FILE *fp = fopen ("PuntajesM.csv","r");
-
-                // Cadena para guardar la linea completa del archivo csv
-                char linea[1024];
-                fgets (linea, 1023, fp);
-                while (fgets (linea, 1023, fp) != NULL){ // Se lee la linea 
-
-                    strcpy(u->nombre, get_csv_field(linea, 0));
-                    u->puntaje = atoi(get_csv_field(linea, 1)); 
-
-                    agregarVideojuego(Marcadores, &(u->puntaje), u);
-                    
-                    u = (Usuario*)malloc(sizeof(Usuario));
-
-                    k++; 
-                    if(k==51) break;
-                }
 
                 mostrar(Marcadores, lastTreeMap(Marcadores), prevTreeMap);
 
@@ -203,6 +246,14 @@ void menu(){
     printf("Introdusca un numero: ");
 }
 
+void submenu(){
+    printf("1. Avanzar\n");
+    printf("2. Mostrar inventario\n");
+    printf("3. Mostrar estado\n");
+    printf("4. Rendirse\n");
+    printf("Introdusca un numero: ");
+}
+
 int lower_than_int(void* key1, void* key2){
     int k1 = *((int*) (key1));
     int k2 = *((int*) (key2));
@@ -236,5 +287,125 @@ void mostrar(TreeMap *mapa, Pair *primero, Pair* (avanzar)(TreeMap *mapa)) {
             u = nextList(lista);
         }
         pair = avanzar(mapa);
+    }
+}
+
+bool esenmigo(NodoGrafo* n){
+    if(n->enemigo == NULL){
+        return false;
+    }
+    return true;
+}
+
+void avanzar(){
+    int opcion;
+    if(grafo->nodoactual == NULL){
+        grafo->nodoactual = grafo->nodoinicial;
+    }
+    else{
+        NodoGrafo* nd = grafo->nodoactual->nododerecha;
+        NodoGrafo* ni = grafo->nodoactual->nodoizquierdo;
+
+        printf("Camino derecho: ");
+        if(esnemigo(nd)){
+            mostrarinfoenemigo(nd->enemigo);
+        }
+        else{
+            motrarinfoobjeto(nd->objeto);
+        }
+
+        printf("Camino izquierdo: ");
+        if(esnemigo(ni)){
+            mostrarinfoenemigo(ni->enemigo);
+        }
+        else{
+            motrarinfoobjeto(ni->objeto);
+        }
+        
+        printf("1. Izquierda o 2. Derecha: ");
+        fflush(stdin);
+        scanf("%d", &opcion);
+        if (opcion == 1){
+            grafo->nodoactual = grafo->nodoactual->nodoizquierdo;
+        }
+        else{
+            grafo->nodoactual = grafo->nodoactual->nododerecha;
+        }
+    }
+
+    if(esnemigo(grafo->nodoactual)){
+        combatir();
+    }
+    else{
+        tomarobjeto();
+    }
+}
+
+
+void Cargarenemigos(HashMap* Map){
+    Enemigo* e = (Enemigo*)malloc(sizeof(Enemigo));
+    int k=0;
+    FILE *fp = fopen ("Enemigos.csv","r");
+    // Cadena para guardar la linea completa del archivo csv
+    char linea[1024];
+    fgets (linea, 1023, fp);
+    while (fgets (linea, 1023, fp) != NULL){ // Se lee la linea 
+
+        strcpy(e->Nombre_Enemigo, get_csv_field(linea, 0)); 
+        e->Dano = atoi(get_csv_field(linea, 1));
+        e->Vida = atoi(get_csv_field(linea, 2));
+        e->nivel_e = atoi(get_csv_field(linea, 3));
+                    
+        insertMap(Map, e->Nombre_Enemigo, e);
+                    
+        e = (Enemigo*)malloc(sizeof(Enemigo));
+
+        k++; 
+        if(k==51) break;
+    }
+}
+
+void Cargarobjetos(HashMap* Map){
+    Objeto* o = (Objeto*)malloc(sizeof(Objeto));
+    int k=0;
+    FILE *fp = fopen ("Objetos.csv","r");
+    // Cadena para guardar la linea completa del archivo csv
+    char linea[1024];
+    fgets (linea, 1023, fp);
+    while (fgets (linea, 1023, fp) != NULL){ // Se lee la linea 
+
+        strcpy(o->Nombre_Item, get_csv_field(linea, 0)); 
+        strcpy(o->Tipo, get_csv_field(linea, 1)); 
+        o->Dano_Cantidad_Armadura = atoi(get_csv_field(linea, 2));
+        o->Nivel_o = atoi(get_csv_field(linea, 3));
+                    
+        insertMap(Map, o->Nombre_Item, o);
+                    
+        o = (Objeto*)malloc(sizeof(Objeto));
+
+        k++; 
+        if(k==51) break;
+    }
+}
+
+void CargarNiveles(Grafo* G){
+    Nivel* n = (Nivel*)malloc(sizeof(Nivel));
+    int k=0;
+    FILE *fp = fopen ("Niveles.csv","r");
+    // Cadena para guardar la linea completa del archivo csv
+    char linea[1024];
+    fgets (linea, 1023, fp);
+    while (fgets (linea, 1023, fp) != NULL){ // Se lee la linea 
+
+        strcpy(n->nombreNivel, get_csv_field(linea, 0)); 
+        n->camino_izq = atoi(get_csv_field(linea, 1));
+        n->camino_der = atoi(get_csv_field(linea, 2));
+                    
+        insertMap(G->Niveles, n->nombreNivel, n);
+                    
+        n = (Nivel*)malloc(sizeof(Nivel));
+
+        k++; 
+        if(k==51) break;
     }
 }
